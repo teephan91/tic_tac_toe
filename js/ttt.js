9525bc7a91f4e3f85d9d43acbfc24ec59eda19e1
Thinh Phan <pseudothinhphan@gmail.com>
2023-07-03 16:35:53 -0700

    Render the gameBoard array to the webpage
    Assign number from 1 to 9 to keep track of elements
    of the array.

--- js/ttt.js
+++ js/ttt.js
@@ -1,5 +1,5 @@
 const gameBoard = (() => {
-    const arr = ["X","X","X","O","O","O","X","X","X"];
+    const arr = ["1","2","3","4","5","6","7","8","9"];
     return {arr};
 })();
 
@@ -14,4 +14,25 @@ const displayController = (() => {
     const playerTwo = Player(getName(), getMark());
 
     return {playerOne, playerTwo};
-})();
+});
+
+function createGameBoard() {
+    const board = document.querySelector('#board');
+    for (let a = 0; a < 3; a++) {
+        const row = document.createElement('div');
+        board.appendChild(row).className = 'row';
+    }
+
+    const rows = document.getElementsByClassName('row');
+    let z = 0;
+    for (let x = 0; x < rows.length; x++) {
+        for (let y = 0; y < 3; y++) {
+            const column = document.createElement('div');
+            column.textContent = gameBoard.arr[z];
+            z++;
+            rows[x].appendChild(column).className = 'column'; 
+        }
+    }
+}
+
+createGameBoard();
