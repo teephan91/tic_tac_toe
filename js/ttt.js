c48bcf616d265c96f22a85cae38c81e4d8c68640
Thinh Phan <pseudothinhphan@gmail.com>
2023-07-05 16:08:58 -0700

    Add click event listener to the gameboard

--- js/ttt.js
+++ js/ttt.js
@@ -1,38 +1,50 @@
-const gameBoard = (() => {
-    const arr = ["1","2","3","4","5","6","7","8","9"];
-    return {arr};
-})();
-
 const Player = (name, mark) => {
     return {name, mark};
 };
 
-const displayController = (() => {
-    const getName = () => prompt('Name?');
-    const getMark = () => prompt('Mark?');
-    const playerOne = Player(getName(), getMark());
-    const playerTwo = Player(getName(), getMark());
+const gameBoard = (() => {
+    const board = ["","","","","","","","",""];
 
-    return {playerOne, playerTwo};
-});
+    function createGameBoard() {
+        const grid = document.querySelector('#grid');
 
-function createGameBoard() {
-    const board = document.querySelector('#board');
-    for (let a = 0; a < 3; a++) {
-        const row = document.createElement('div');
-        board.appendChild(row).className = 'row';
-    }
+        for (let a = 0; a < 3; a++) {
+            const row = document.createElement('div');
+            grid.appendChild(row).className = 'row';
+        }
+    
+        const rows = document.getElementsByClassName('row');
+        let z = 0;
 
-    const rows = document.getElementsByClassName('row');
-    let z = 0;
-    for (let x = 0; x < rows.length; x++) {
-        for (let y = 0; y < 3; y++) {
-            const column = document.createElement('div');
-            column.textContent = gameBoard.arr[z];
-            z++;
-            rows[x].appendChild(column).className = 'column'; 
+        for (let x = 0; x < rows.length; x++) {
+            for (let y = 0; y < 3; y++) {
+                const column = document.createElement('div');
+                column.textContent = gameBoard.board[z];
+                z++;
+                rows[x].appendChild(column).className = 'column'; 
+            }
         }
     }
-}
 
-createGameBoard();
+    function addMarks() {
+        const squares = document.getElementsByClassName('column');
+
+        for (let square of squares) {
+            square.addEventListener('click', () => alert('Clicked!'));
+        }
+    }
+
+    return {board, createGameBoard, addMarks};
+})();
+
+gameBoard.createGameBoard();
+gameBoard.addMarks();
+
+// const displayController = (() => {
+//     const getName = () => prompt('Name?');
+//     const getMark = () => prompt('Mark?');
+//     const playerOne = Player(getName(), getMark());
+//     const playerTwo = Player(getName(), getMark());
+
+//     return {playerOne, playerTwo};
+// })();
